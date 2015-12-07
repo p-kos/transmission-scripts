000fc370a87684bcc451f9d2048f6df9ee8077be
Marco Zarate <pkitos@gmail.com>
2015-12-07 06:37:30 -0400

    using variables instead of hardcoded names

--- onfinish.sh
+++ onfinish.sh
@@ -2,9 +2,11 @@
 USER=pkos
 PASS=22022009
 PORT=9091
+TARGET_FOLDER=/mnt/Media/New/
+LOGFILE=/home/pi/downloaded.log
 if [ -d /mnt/Media/New ]; then
-  echo "\"$TR_TORRENT_NAME\" on \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> /home/pi/downloaded.log
-  cp -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" /mnt/Media/New/ >> /home/pi/downloaded.log
+  echo "\"$TR_TORRENT_NAME\" on \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> $LOGFILE
+  cp -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" $TARGET_FOLDER >> $LOGFILE
   transmission-remote $PORT --auth $USER:$PASS --torrent $TR_TORRENT_ID --remove
   rm -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" 
 fi
