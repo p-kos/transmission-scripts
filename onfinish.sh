d5a17d9341f86864c197fb2bb3c24075059a2014
Marco Zarate <pkitos@gmail.com>
2016-06-01 05:56:11 -0400

    adjusted messages

--- onfinish.sh
+++ onfinish.sh
@@ -3,10 +3,12 @@ USER=pkos
 PASS=22022009
 PORT=9091
 TARGET_FOLDER=/mnt/Media/New/
-LOGFILE=/home/pi/downloaded.log
-if [ -d /mnt/Media/New ]; then
-  echo "\"$TR_TORRENT_NAME\" on \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> $LOGFILE
-  cp -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" $TARGET_FOLDER >> $LOGFILE
+LOGFILE=/home/osmc/downloaded.log
+if [ -d "$TARGET_FOLDER" ]; then
+  echo "\"$TR_TORRENT_DIR/$TR_TORRENT_NAME\" at \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> $LOGFILE
   transmission-remote $PORT --auth $USER:$PASS --torrent $TR_TORRENT_ID --remove
-  rm -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" 
+  cp -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME" $TARGET_FOLDER >> $LOGFILE  
+  if [ -d "$TARGET_FOLDER/$TR_TORRENT_NAME"]; then
+    rm -fr "$TR_TORRENT_DIR/$TR_TORRENT_NAME"
+  fi 
 fi
