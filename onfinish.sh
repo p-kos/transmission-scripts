b3551c18496bd1e0bd2308e82b4e058511cc9f43
Marco Zarate <pkitos@gmail.com>
2016-06-04 16:32:49 -0400

    removed the  creation of file

--- onfinish.sh
+++ onfinish.sh
@@ -4,9 +4,6 @@ PASS=22022009
 PORT=9091
 TARGET_FOLDER=/mnt/Media/New/
 LOGFILE=/home/osmc/downloaded.log
-if [ ! -f $LOGFILE]; then
-  echo "Transmission download log" > $LOGFILE
-fi
 if [ -d "$TARGET_FOLDER" ]; then
   echo "\"$TR_TORRENT_DIR/$TR_TORRENT_NAME\" at \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> $LOGFILE
   transmission-remote $PORT --auth $USER:$PASS --torrent $TR_TORRENT_ID --remove
