2e17e76206b36929492d3877dbbd5ead087b040a
Marco Zarate <pkitos@gmail.com>
2016-07-03 07:03:59 -0400

    changed home

--- onfinish.sh
+++ onfinish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-USER=pkos
-PASS=22022009
+USER=transmission
+PASS=XXXX
 PORT=9091
 TARGET_FOLDER=/mnt/Media/New/
-LOGFILE=/home/osmc/downloaded.log
+LOGFILE=~/downloaded.log
 if [ -d "$TARGET_FOLDER" ]; then
   echo "\"$TR_TORRENT_DIR/$TR_TORRENT_NAME\" at \"$TR_TIME_LOCALTIME\" (id: $TR_TORRENT_ID)" >> $LOGFILE
   transmission-remote $PORT --auth $USER:$PASS --torrent $TR_TORRENT_ID --remove
